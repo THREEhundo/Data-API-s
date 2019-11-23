4c42d424e02d32b71d0afd0870d00fb9e52504ea
Samuel Baik <samuelbaik87@gmail.com>
2019-11-23 18:24:17 -0500

    Map and tiles created

--- fetching-JSON-ISS/ISS.js
+++ fetching-JSON-ISS/ISS.js
@@ -1,7 +1,14 @@
 const url = 'https://api.wheretheiss.at/v1/satellites/25544'
 
 // Create map and tiles
+const mymap = L.map('issMap').setView([0, 0], 1);
+//                                      ^ lat long & zoom
+const attribution =
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 
+const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+const tiles = L.tileLayer(tileURL, {attribution})
+tiles.addTo(mymap)
 // Create marker with a custom icon
 
 // fetching ISS json data
