efa74b49bd94580db09006690f8ef1472f03f4b5
Samuel Baik <samuelbaik87@gmail.com>
2019-11-23 19:15:30 -0500

    Added marker to map

--- fetching-JSON-ISS/ISS.js
+++ fetching-JSON-ISS/ISS.js
@@ -1,4 +1,3 @@
-const url = 'https://api.wheretheiss.at/v1/satellites/25544'
 
 // Create map and tiles
 const mymap = L.map('issMap').setView([0, 0], 1);
@@ -12,11 +11,15 @@ tiles.addTo(mymap)
 // Create marker with a custom icon
 
 // fetching ISS json data
+const url = 'https://api.wheretheiss.at/v1/satellites/25544'
+
 async function dataISS() {
   const response = await fetch(url)
   const apiData = await response.json()
-
   const {latitude, longitude, velocity} = apiData
+
+  L.marker([latitude, longitude]).addTo(mymap)
+
   document.getElementById('latitude').textContent = latitude
   document.getElementById('longitude').textContent = longitude
   document.getElementById('velocity').textContent = velocity
