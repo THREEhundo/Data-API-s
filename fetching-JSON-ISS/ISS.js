d25f2dc89c8c0f3d26be9e862408076f7a15c139
Samuel Baik <samuelbaik87@gmail.com>
2019-11-25 18:55:24 -0500

    removed if statement from dataISS() set zoom on mymap changed icon size

--- fetching-JSON-ISS/ISS.js
+++ fetching-JSON-ISS/ISS.js
@@ -1,6 +1,6 @@
 
 // Create map and tiles
-const mymap = L.map('issMap').setView([0, 0], 1);
+const mymap = L.map('issMap').setView([0, 0], 4);
 //                                      ^ lat long & zoom
 
 // Attributions
@@ -14,7 +14,7 @@ tiles.addTo(mymap)
 // Create marker with a custom icon
 const iss400 = L.icon({
     iconUrl: 'iss400.png',
-    iconSize: [50, 32],
+    iconSize: [75, 50],
     iconAnchor: [25, 16],
 })
 const marker = L.marker([0, 0], { icon: iss400 }).addTo(mymap)
@@ -22,19 +22,15 @@ const marker = L.marker([0, 0], { icon: iss400 }).addTo(mymap)
 // fetching ISS json data
 const url = 'https://api.wheretheiss.at/v1/satellites/25544'
 
-let firstTime = true
-
 async function dataISS() {
   const response = await fetch(url)
   const apiData = await response.json()
   const {latitude, longitude, velocity} = apiData
 
 // Zoom
+  mymap.setView([latitude, longitude], mymap.getZoom());
   marker.setLatLng([latitude, longitude])
-  if (firstTime) {
-    mymap.setView([latitude, longitude], 4)
-    firstTime = false
-  }
+
   document.getElementById('longitude').textContent = longitude.toFixed(2)
   document.getElementById('latitude').textContent = latitude.toFixed(2)
   document.getElementById('velocity').textContent = velocity.toFixed(2)
