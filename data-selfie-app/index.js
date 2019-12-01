8de10eb1cc11defbdcb03a2452c42d2157923147
Samuel Baik <samuelbaik87@gmail.com>
2019-12-01 17:14:31 -0500

    created a database.

--- data-selfie-app/index.js
+++ data-selfie-app/index.js
@@ -1,19 +1,28 @@
 const express = require('express')
+const Datastore = require('nedb') //Database
+
 const app = express()
 app.listen(3000, () => console.log('Listening at 3000'))
 app.use(express.static('public'))
-
 app.use(express.json({ limit: '1mb' }))
 // To run use Git Bash
 // node index.js
 
+const db = new Datastore('database.db')
+db.loadDatabase()
+// loads the previous data available when the server ran into memory, if the database hasnt been created.
+
 app.post('/api', (request, response) => {
-  console.log(`I got a request`)
-  console.log(request.body)
   const data = request.body
+  const timestamp = Date.now()
+  data.timestamp = timestamp
+  db.insert(data)
+  console.log(data)
   response.json({
     status: 'success',
+    timestamp: timestamp,
     latitude: data.latitude,
     longitude: data.longitude
   })
 })
+// Callback to get and give data.
