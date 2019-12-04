a007ff6f69ef04f2370b1d016bf4e751cf90c9fe
Samuel Baik <samuelbaik87@gmail.com>
2019-12-04 13:40:05 -0500

    data
    data

--- data-selfie-app/index.js
+++ data-selfie-app/index.js
@@ -15,28 +15,67 @@ db.loadDatabase()
 const userDB = new Datastore('users.db')
 userDB.loadDatabase()
 
+// allDATA.html
+app.get('/api', (request, response) => {
+  db.find({}, (err, data) => {
+    if (err) {
+      response.end()
+      return
+    }
+    response.json(data)
+  })
+})
+
+app.get('/userData', (request, response) => {
+  userDB.find({}, (err, data) => {
+    if (err) {
+      response.end()
+      return
+    }
+    response.json(data)
+    console.log(`yo`)
+  })
+})
+
+
+app.delete('/api', (request, response) => {
+  db.find({ name: { $exists: true} }, (err, data) => {
+    if (err) {
+      response.end()
+      return
+    }
+    response.json(request.data)
+    // console.log(`yo`)
+  })
+})
+
+// app.delete('/api', (request, response) => {
+//   const deleting = db.find({ name: { $exists: true } }, (err, data) => {
+//     if (err) {
+//       console.log('error');
+//       response.end()
+//       return
+//     }
+//     console.log('works');
+//     const index = data.indexOf(request.body)
+//     db.splice(index, 1)
+//   })
+//   })
+  // if json object key contains name delete object
+
+// index.html
 app.post('/api', (request, response) => {
   const data = request.body
   const timestamp = Date.now()
   data.timestamp = timestamp
   db.insert(data)
   console.log(data)
-  response.json({
-    status: 'success',
-    timestamp: timestamp,
-    latitude: data.latitude,
-    longitude: data.longitude
-  })
+  response.json(data)
 })
 
 app.post('/userData', (request, response) => {
   const data = request.body
   userDB.insert(data)
-  response.json({
-    status: 'success',
-    name: data.name,
-    email: data.mail,
-    message: data.msg
-  })
+  response.json(data)
 })
 // Callback to get and give data.
